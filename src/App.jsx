089bfaf23e3bddd8301e96ab92ee7edd7ded7f2a
smacknfor10 <smacknfor1@gmail.com>
2025-08-02 17:40:22 +0200

    Testimonials Section

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+
+const App = () => {
+    return (
+      <div>
+        <Navbar />
+        <Hero />
+        <div className='container'>
+          <Title subTitle='Our PROGRAM' title='What We Offer' />
+          <Programs />
+          <About />
+          <Title subTitle='Gallery' title='Campus Photos' />
+          <Campus />
+          <Title subTitle='TESTINOMIALS' title='What Student Says' />
+          <Testimonials />
+        </div>
+      </div>
+    )
+}
+
+export default App
